e3cef5a5f202f40b71206aeb922bc67c2c21a76b
queuebit <queuebit10648@gmail.com>
2013-12-27 20:47:46 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '75dfb41209cb8b96a0621146629c03332b262daa3b62823400260118d55f6b4fb84678d51769345af5ddc79f4a232b59832fc2f3777aa36dbb45eada770a18fd'
